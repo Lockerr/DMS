831c9f845ee9f0b19c7b055f508d1f9567b94918
renius <antiqe@gmail.com>
2012-06-18 10:53:27 +0600

    fix

--- app/models/document.rb
+++ app/models/document.rb
@@ -156,13 +156,18 @@ class Document
     system "rm #{Rails.root.join('tmp', temp, 'word', 'footer1.xml')}"
     system "rm #{Rails.root.join('tmp', temp, 'word', 'footer2.xml')}"
 
-    file = File.new(Rails.root.join('tmp', temp, 'docProps', 'custom.xml'), 'w').write(skeleton)
+    file = File.new(Rails.root.join('tmp', temp, 'docProps', 'custom.xml'), 'w')
+    puts "sekeleton #{file.write(skeleton)}"
     file.close
-    file = File.new(Rails.root.join('tmp', temp, 'word', 'document.xml'), 'w').write(docbody.to_s)
+    file = File.new(Rails.root.join('tmp', temp, 'word', 'document.xml'), 'w')
+    puts "document #{file.write(docbody.to_s)}"
     file.close
-    file = File.new(Rails.root.join('tmp', temp, 'word', 'footer1.xml'), 'w').write(footer_1.to_s)
+    file = File.new(Rails.root.join('tmp', temp, 'word', 'footer1.xml'), 'w')
+    puts "footer1 #{file.write(footer_1.to_s)}"
     file.close
-    file = File.new(Rails.root.join('tmp', temp, 'word', 'footer2.xml'), 'w').write(footer_2.to_s)
+    file = File.new(Rails.root.join('tmp', temp, 'word', 'footer2.xml'), 'w')
+    puts "footer 2 #{file.write(footer_2.to_s)}"
+
     file.close
 
     Dir.mkdir "/var/www/fpk/upload/files/clients/#{client.id}/" unless Dir.exists? "/var/www/fpk/upload/files/clients/#{client.id}/"
