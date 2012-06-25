e5d9c8b254a3fa035a3e53ead431e1db4d7efc03
renius <antiqe@gmail.com>
2012-06-25 17:34:06 +0600

    fix

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,9 +3,14 @@ if Rails.env == 'production'
   unless self.private_methods.include? 'irb_binding'
     task_scheduler = Rufus::Scheduler.start_new(:frequency => 0.5)
 
+    task_scheduler.in('1s') do
+      Rails.logger.info 'Sheduler initiated'
+    end
+
     task_scheduler.every('1m') do
       Order.strore
 
     end
+
   end
 end
