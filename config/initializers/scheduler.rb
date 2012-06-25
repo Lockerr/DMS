386668baf5e2038151c177d08651e6470e773263
renius <antiqe@gmail.com>
2012-06-25 17:31:35 +0600

    fix

---
+++ config/initializers/scheduler.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+if Rails.env == 'production'
+  unless self.private_methods.include? 'irb_binding'
+    task_scheduler = Rufus::Scheduler.start_new(:frequency => 0.5)
+
+    task_scheduler.every('1m') do
+    end
+  end
+end
