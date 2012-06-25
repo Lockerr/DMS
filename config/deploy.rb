a16e922d8e92f73c5d8af429ecdf40a71f69b75b
renius <antiqe@gmail.com>
2012-06-25 16:05:58 +0600

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "crm"
-set :repository,  "svn://192.168.1.71/home/anton/work/dms/"
+set :repository,  "192.168.1.71/home/anton/work/dms/"
 set :domain, 'user@192.168.1.98'
 set :password, 'ktghfpjhbq'
 set :deploy_to, '/home/user/dms/'
