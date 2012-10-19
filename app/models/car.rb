060794fada79e6f2c0fabfeaa5e8fbd463df145f
renius <antiqe@gmail.com>
2012-10-19 11:31:40 +0600

    fix

--- app/models/car.rb
+++ app/models/car.rb
@@ -27,7 +27,7 @@ class Car < ActiveRecord::Base
   def self.mbr
     require "selenium-webdriver"
     profile = Selenium::WebDriver::Firefox::Profile.new
-    profile['browser.download.dir'] = Rails.root.join('tmp','mbr').to_s
+    profile['browser.download.dir'] = Rails.root.join('tmp').to_s
     profile['browser.download.folderList'] = 2
     profile['browser.helperApps.neverAsk.saveToDisk'] = "application/vnd.ms-excel"
 
@@ -114,7 +114,7 @@ class Car < ActiveRecord::Base
     }
     sleep 5
     
-    Dir.chdir(Rails.root.join('tmp','mbr'))
+    Dir.chdir(Rails.root.join('tmp'))
     file = File.new Rails.root.join('tmp','mbr', Dir.glob('*.xls')[-1]).to_s, 'r'
     puts file.inspect
     parse_cars(file)
