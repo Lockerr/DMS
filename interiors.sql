d90b55967b10ca28b771276b1ce7f9f554c62e2a
renius <antiqe@gmail.com>
2012-10-24 16:54:07 +0600

    document_markup

--- interiors.sql
+++ interiors.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.61, for debian-linux-gnu (i686)
+-- MySQL dump 10.13  Distrib 5.1.63, for debian-linux-gnu (i686)
 --
 -- Host: localhost    Database: dms.ext_dev
 -- ------------------------------------------------------
--- Server version	5.1.61-0ubuntu0.10.10.1
+-- Server version	5.1.63-0ubuntu0.11.10.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -50,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-06-21 16:06:29
+-- Dump completed on 2012-10-24 16:17:06
