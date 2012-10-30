1a5a9e6e776bf68eeb86c1937ab5a0a8dc1ecfc3
renius <antiqe@gmail.com>
2012-10-30 11:48:22 +0600

    fix

--- app/models/document.rb
+++ app/models/document.rb
@@ -95,8 +95,9 @@ class Document
     end
     
     
-
-    docbody.root.elements["*/w:p/w:fldSimple[@w:instr=' DOCPROPERTY  person_name_2  \\* MERGEFORMAT ']"].elements['w:r'].elements['w:t'].text = (attrs[:person_name].to_s || ' ')
+    unless object.class.to_s.downcase == 'dkp'
+      docbody.root.elements["*/w:p/w:fldSimple[@w:instr=' DOCPROPERTY  person_name_2  \\* MERGEFORMAT ']"].elements['w:r'].elements['w:t'].text = (attrs[:person_name].to_s || ' ')
+    end
 
     if object.class.to_s.downcase == 'contract'
       docbody.root.elements["*/w:p/w:fldSimple[@w:instr=' DOCPROPERTY  car_model_name_2  \\* MERGEFORMAT ']"].elements['w:r'].elements['w:t'].text = (attrs[:car_model_name].to_s || ' ')
@@ -211,6 +212,7 @@ class Document
     doc_filename_name = case object.class.to_s.downcase
       when 'contract' then 'Договор'
       when 'act' then 'Акт'
+      when 'dkp' then 'Договор купли-продажи'
     end
     
     system("cd #{temp} && zip /var/www/fpk/upload/files/clients/#{client.id}/#{doc_filename_name}.docx -r  .")
